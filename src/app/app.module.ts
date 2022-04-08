2bd6b1488e699cf4a4db53fd7276e94604c9a3cd
edwardmoche <edwardmoche@yahoo.com>
2022-04-08 11:15:37 +0700

    TodoApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { navbarComponent } from './components/navbar/navbar.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { AddTodoFormComponent } from './components/add-todo-form/add-todo-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    navbarComponent,
+    TodoListComponent,
+    AddTodoFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
